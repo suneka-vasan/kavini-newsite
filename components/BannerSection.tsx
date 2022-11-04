c5ad3d2543836eabb52e614f4b1ebf2d8469f0d5
Suneka-vasan <suneka@glenise.in>
2022-11-04 13:33:53 +0530

    update 5-1
    Signed-off-by: Suneka-vasan <suneka@glenise.in>

--- components/BannerSection.tsx
+++ components/BannerSection.tsx
@@ -10,9 +10,9 @@ function BannerSection() {
                 backgroundSize: 'cover',
                 backgroundRepeat: 'no-repeat'
             }} >
-            <Box maxW='container.xl' mx={{ base: 0, md: 'auto' }} pt={{ base: 24, md: 60 }} pl={{ base: 5, md: 4, lg: 0 }}>
+            <Box maxW='container.xl' mx={{ base: 0, md: 'auto' }} pt={{ base: 36, md: 60 }} pl={{ base: 5, md: 4, lg: 0 }}>
                 <Text color="#033303" pb={0}>Weekend Sale!</Text>
-                <Heading color="#033303" pb={5}>Discover Spring Collection</Heading>
+                <Heading color="#033303" pb={5} fontSize={{ base: "24px", md: "36px" }}>Discover Spring Collection</Heading>
                 <PrimaryButtonLg title='Shop Now' path='/about' />
             </Box>
         </Box >
